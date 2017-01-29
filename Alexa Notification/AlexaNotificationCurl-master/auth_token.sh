2fc87353f66332a690869bde4e3779a4f9417fd1
Ubuntu <ubuntu@ip-172-31-56-117.ec2.internal>
2017-01-29 07:53:23 +0000

    generated audio files

--- Alexa Notification/AlexaNotificationCurl-master/auth_token.sh
+++ Alexa Notification/AlexaNotificationCurl-master/auth_token.sh
@@ -1,6 +1,6 @@
 CLIENT_ID="amzn1.application-oa2-client.6174d75e2c0144a1bd0f1964fe712bb1"
 CLIENT_SECRET="dae7ff77c1777b638fd53ab1960376fb89f8652c8bafc2099b251fe15f08d25c"
-CODE="ANkRFMOWsNYKWnLZCQaa"
+CODE="ANROiiRMZEGXUOeKJVjX"
 GRANT_TYPE="authorization_code"
 REDIRECT_URI="https://localhost:9745/authresponse"
 
