304292fb69e7e690c395909ec55dcbe86abb3600
Killian-LeClainche <kleclain@purdue.edu>
2017-01-29 11:44:42 -0600

    stuff

--- src/com/nebby/server/Server.java
+++ src/com/nebby/server/Server.java
@@ -13,12 +13,58 @@ public class Server
 
 	public static void main(String[] args) throws Exception {
 		HttpServer server = HttpServer.create(new InetSocketAddress(8888), 0);
-		server.createContext("/test", new MyHandler());
+		server.createContext("/addPill", new AddPill());
+		server.setExecutor(null); // creates a default executor
+		server.start();
+		
+		server.createContext("/takePill", new TakePill());
+		server.setExecutor(null); // creates a default executor
+		server.start();
+		
+		server.createContext("/pillsTaken", new PillsTaken());
+		server.setExecutor(null); // creates a default executor
+		server.start();
+		
+		server.createContext("/clear", new Clear());
 		server.setExecutor(null); // creates a default executor
 		server.start();
 	}
 
-	static class MyHandler implements HttpHandler 
+	static class AddPill implements HttpHandler 
+	{
+		public void handle(HttpExchange t) throws IOException 
+		{
+			String response = "Welcome Real's HowTo test page";
+			t.sendResponseHeaders(200, response.length());
+			OutputStream os = t.getResponseBody();
+			os.write(response.getBytes());
+			os.close();
+		}
+	}
+	
+	static class TakePill implements HttpHandler 
+	{
+		public void handle(HttpExchange t) throws IOException {
+			String response = "Welcome Real's HowTo test page";
+			t.sendResponseHeaders(200, response.length());
+			OutputStream os = t.getResponseBody();
+			os.write(response.getBytes());
+			os.close();
+		}
+	}
+	
+	static class PillsTaken implements HttpHandler 
+	{
+		public void handle(HttpExchange t) throws IOException {
+			String response = "Welcome Real's HowTo test page";
+			t.sendResponseHeaders(200, response.length());
+			OutputStream os = t.getResponseBody();
+			os.write(response.getBytes());
+			os.close();
+		}
+	}
+	
+	static class Clear implements HttpHandler 
 	{
 		public void handle(HttpExchange t) throws IOException {
 			String response = "Welcome Real's HowTo test page";
