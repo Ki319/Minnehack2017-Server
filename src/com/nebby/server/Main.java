d8eb433d108f0aec60b911dcbdfd2e136b9c4cd2
Killian-LeClainche <kleclain@purdue.edu>
2017-01-29 11:35:25 -0600

    stuff

--- src/com/nebby/server/Main.java
+++ src/com/nebby/server/Main.java
@@ -1,24 +1,29 @@
 package com.nebby.server;
 
 import java.io.IOException;
+import java.io.InputStream;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
 
 public class Main 
 {
     public static void main(String argv[]) throws ClientProtocolException, IOException
     {
-        String url = "http://ec2-54-172-226-18.compute-1.amazonaws.com:8888/test";
+        String url = "http://127.0.0.1:8888/test";
 
         HttpClient client = HttpClientBuilder.create().build();
         HttpGet request = new HttpGet(url);
 
 
         HttpResponse response = client.execute(request);
-        System.out.println("http response = "+response.toString());
+        
+        String body = EntityUtils.toString(response.getEntity(), "UTF-8");
+        System.out.println("http response = "+body);
     }
 }
